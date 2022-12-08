3ff72f6a8ee1cbca08bf74413488ff885a2280ed
Nolen Lawton <mnlawton@icloud.com>
2022-12-08 13:37:38 -0600

    created conditional for arguments (#13)

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -40,13 +40,26 @@ var pets = 3;
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 
+var friendsPets = 1;
+
+
 // 11 - Add two pets to your `pets` variable
 
+// Not sure how to get both
+
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+
+const allowedPets = 2;
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 
+if(adventurous){
+    console.log('Adventures are great!')
+}
+else{
+    console.log('How about we stay home?')
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
