e04c21bca497cc1046a1e1d3eb929df9bd93c1e4
Nolen Lawton <mnlawton@icloud.com>
2022-12-11 14:23:10 -0600

    completed if statement to check for max as well as created switch statement

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -36,7 +36,7 @@ let food = "pizza";
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 
-let pets = 3;
+let pets = 1;
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 
@@ -48,7 +48,7 @@ pets += 2;
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
-const allowedPets = 2;
+const allowedPets = 3;
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
@@ -91,12 +91,33 @@ else{
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
 
+let mostPets
+
+if(pets && friendsPets){
+    mostPets = Math.max(pets, friendsPets);
+    console.log(mostPets);
+}
+
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+
+switch(luckyNumber){
+    case(1):
+    console.log("First is the worst");
+    break;
+    case(2):
+    console.log("Second is the best");
+    break;
+    case(3):
+    console.log("Third is the one with the polka dot dress");
+    break;
+    default:
+    console.log("Luck is what happens when preparation meets opportunity");
+}
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
